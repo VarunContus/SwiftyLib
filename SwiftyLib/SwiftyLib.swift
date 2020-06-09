0f798737871948194cb1e1450e93cabc0c50fadd
Varunpv_Contus <varun.p@contus.in>
2020-06-09 12:05:43 +0530

    added test cases

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by user on 09/06/20.
+//  Copyright © 2020 contus. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
